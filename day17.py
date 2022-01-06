1c114b56c114125f4790c8ca97d70a04c35df865
Alex Tan Hong Pin <alextan220990@gmail.com>
2022-01-06 21:08:49 +0800

    Update day17.py

--- day17.py
+++ day17.py
@@ -9,11 +9,25 @@ def spin(insertions: int, steps: int, target: int) -> int:
     for i in range(1, insertions + 1):
         spinlock.rotate(-steps)
         spinlock.append(i)
-    while spinlock[0] != target:
-        spinlock.rotate(1)
-    spinlock.rotate(-1)
+    tgt = spinlock.index(target)
+    spinlock.rotate(tgt + 1)
     return spinlock[0]
 
 
 print(spin(2017, steps, 2017))
-print(spin(50000000, steps, 0))
+
+
+# 0 stays at the same position, so we only check any item that is placed after
+# it
+curr = 0
+length = 1
+last = 0
+for i in range(50000000):
+    curr = (curr + steps) % length
+    curr += 1
+    if curr == 1:
+        last = i
+    length += 1
+
+
+print(last)
